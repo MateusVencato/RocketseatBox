10de350f45474eb48e6c446e67534ba2e60f3807
Mateus Vencato <mateus.vencato@pmweb.local>
2019-04-09 14:29:22 -0300

    continuacao do projeto

--- src/server.js
+++ src/server.js
@@ -1,6 +1,13 @@
 const express = require('express');
+const mongoose = require('mongoose');
 
 const app = express();
+
+mongoose.connect('mongodb+srv://omnistack:<omnistack>@cluster0-zfgqu.mongodb.net/omnistack?retryWrites=true', 
+{
+    useNewUrlParser: true
+}
+);
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
