48a43c9e43f261a5f9cff07b0baaa814d6139c0a
Mateus Vencato <mateus.vencato@pmweb.local>
2019-04-11 09:12:53 -0300

    teste xyz

--- src/routes.js
+++ src/routes.js
@@ -10,6 +10,12 @@ const FileController = require('./controllers/FileController');
 routes.post("/boxes", BoxController.store);
 routes.get("/boxes/:id", BoxController.show);
 
-routes.post('/boxes/:id/files', multer(multerConfig).single('file'), FileController.store);
+routes.post('/boxes/:id/files', (data) => {
+    console.log(data);
+} );
+
+routes.post('/xyz', (data) => {
+    console.log(data);
+} );
 
 module.exports = routes;
